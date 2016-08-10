343f29f8d464d054f0ed1e00569b76c072e235ae
Konstantinos Goutsos <kgoutsos@outlook.com>
2016-08-10 12:37:05 +0100

    replaced natural language date parsing library with a better one

--- res.py
+++ res.py
@@ -1,6 +1,7 @@
 import sys
 import os
-import dateparser
+import parsedatetime
+from datetime import datetime
 import subprocess
 from pytodo import duedate as dd
 from pytodo import task as tt
@@ -18,15 +19,14 @@ task_date = dd.get_due_date(task)
 if new_date_arg == "none":
     task = dd.remove_due_date(task)
 else:
-    new_date = dateparser.parse(sys.argv[2])
-    if new_date == None:
+    cal = parsedatetime.Calendar()
+    time_struct, parse_status = cal.parse(sys.argv[2])
+    if parse_status == 1:
+        new_date = datetime(*time_struct[:6]).date()
+        if task_date == new_date:
+            print("The new date is the same as the old one")
+        else:
+            task = dd.set_due_date(task, new_date)
+            tt.update_task(task_no, task)
+    else:
         print("New date could not be parsed")
-        exit()
-    new_date = new_date.date()
-    if task_date == new_date:
-        print("The new date is the same as the old one")
-        exit()
-
-    task = dd.set_due_date(task, new_date)
-
-tt.update_task(task_no, task)
