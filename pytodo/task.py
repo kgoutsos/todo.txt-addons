107273186114455ddfd15c4c145c8e5b4e8566b5
Konstantinos Goutsos <kgoutsos@outlook.com>
2016-12-04 10:04:29 +0000

    fixed colorisation bugs

--- pytodo/task.py
+++ pytodo/task.py
@@ -3,14 +3,14 @@
 import subprocess
 import os
 
-TODO_SH = "{0} -p -d {1}".format(os.environ['TODO_FULL_SH'], os.environ['TODOTXT_CFG_FILE'])
+TODO_SH = "{0} -d {1}".format(os.environ['TODO_FULL_SH'], os.environ['TODOTXT_CFG_FILE'])
 
 def get_task(task_no):
-    cmd = "{:} ls".format(TODO_SH)
+    cmd = "{:} -p ls".format(TODO_SH)
     lines = subprocess.check_output(cmd, shell=True).split('\n')
     task = None
     for l in lines:
-        parts = l.split(' ')    
+        parts = l.split(' ')
         if len(parts) < 2:
             continue
         if int(parts[0]) == int(task_no):
@@ -36,7 +36,7 @@ def add_task(task, due_date = None, duplicate = True, checkDuplicateDone = False
         print(task)
         print("Task already exists. Nothing added.")
         return
-    cmd = "{:} add {:}".format(TODO_SH, task)
+    cmd = "{:} add \"{:}\"".format(TODO_SH, task)
     print(subprocess.check_output(cmd, shell=True).strip())
 
 def update_task(task_no, new_task):
